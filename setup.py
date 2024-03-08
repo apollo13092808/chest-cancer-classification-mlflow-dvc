24a0f812a3ea2f0a4fc1c780e5bb5e3f572ecef3
An Vo <tuananvo1309@gmail.com>
2024-03-08 09:42:25 +0700

    Reconfig project structure

--- setup.py
+++ setup.py
@@ -7,23 +7,23 @@ with open(file="README.md", mode="r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "chest-cancer-classification-mlflow-dvc"
-AUTHOR_USERNAME = config("GITHUB_USERNAME")
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = config("GITHUB_EMAIL")
+GITHUB_REPO_NAME = config("GITHUB_REPO_NAME")
+GITHUB_USER_NAME = config("GITHUB_USER_NAME")
+GITHUB_USER_EMAIL = config("GITHUB_USER_EMAIL")
 
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
-    author=AUTHOR_USERNAME,
-    author_email=AUTHOR_EMAIL,
+    author=GITHUB_USER_NAME,
+    author_email=GITHUB_USER_EMAIL,
     description="A small python package for CNN app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}",
+    url=f"https://github.com/{GITHUB_USER_NAME}/{GITHUB_REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{GITHUB_USER_NAME}/{GITHUB_REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
